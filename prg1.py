76c58d850e71f30b4c91efeb53ff780554b4addb
Aliyas Shaik <aliyasshaik7@gmail.com>
2020-07-26 19:18:07 -0700

    Commit

---
+++ prg1.py
@@ -0,0 +1,32 @@
+#print("Aialz Technologies")
+
+#print("aliyas \nShaik")
+#print('Aliyas Shaik')
+#print(''Aliyas Shaik'')
+#print ('''Aliyas Shaik''')
+#print("Aliyas Shaik")
+#print(""Aliyas Shaik"")
+#print ("""Aliyas Shaik""")
+print ("""Aliyas 
+Shaik""")
+#use end to join individual pieces into a big blob of text with a custom separator. Instead of joining multiple arguments, however, it’ll append text from each function call to the same line
+print('The first sentence', end='. ')
+print('The second sentence', end='. ')
+print('The last sentence.')
+print('Aliyas \nShaik')
+print(None)
+#print(x)
+print('hello', 'world', sep='')
+print('hello', 'world', sep=None)
+print('hello', 'world', sep='     ')
+print('hello', 'world')
+print('21', 'July', '2020', sep='/')
+
+print(17, 'RT Nagar', 'Bangalore', 'Karnataka', sep=',')
+print(17, 'RT Nagar', 'Bangalore', 'Karnataka')
+
+value = print('hello world')
+
+print(value)
+import os
+print('Hello, ' + os.getlogin() + '! How are you?')
